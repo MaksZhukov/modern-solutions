d9e89e29d95642ab2c84fefaf222895e16d3a3fa
MaksZhukov <maks_zhukov_97@mail.ru>
2021-09-16 23:38:25 +0300

    Added slider

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,40 @@
 import Layout from '../layouts/Layout';
 import Button from '../components/Button/Button';
+import Slider from 'react-slick';
 // import client from '../database';
+import Image from 'next/image';
 import styles from './index.module.scss';
+import Container from '../components/Container/Container';
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+
+const clientImages = Array(10)
+    .fill()
+    .map((item, index) => `/client-${index + 1}.jpg`);
 
 export default function Home() {
     return (
         <Layout>
-            <div className='container'>
-                Home <Button>Подробне</Button>
-            </div>
+            <Container>
+                <h2>НАШИ КЛИЕНТЫ</h2>
+                <Slider
+                    centerMode
+                    arrows
+                    infinite
+                    speed={500}
+                    slidesToScroll={2}
+                    slidesToShow={3}
+                    className={styles.clients}>
+                    {clientImages.map((item, index) => (
+                        <div className={styles['clients__item']}>
+                            <img
+                                className={styles['clients__img']}
+                                src={`client-${index + 1}.jpg`}
+                            />
+                        </div>
+                    ))}
+                </Slider>
+            </Container>
         </Layout>
     );
 }
