30e61897596a1b1a103846cc6e8af4e81e0ec5c3
Maksim Zhukau <Maksim.Zhukau@itechart-group.com>
2022-06-17 15:15:10 +0300

    Added right smtp settings

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,32 +1,36 @@
 import { SMTPClient } from 'emailjs';
 
 const client = new SMTPClient({
-	user: 'makszhukov1997@gmail.com',
-	password: 'ytngfhjkz199719971997',
-	host: 'smtp.gmail.com',
-	port: 587,
-
-	ssl: true,
+    user: process.env.SMTP_USER,
+    password: process.env.SMTP_PASSWORD,
+    host: 'smtp.yandex.ru',
+    port: 465,
+    ssl: true
 });
 
 export default function handler(req, res) {
-	if (req.method === 'POST') {
-		const { name, phone, text } = req.body;
-		client.send(
-			{
-				text: text,
-				from: `${name} ${phone}`,
-				to: '<makszhukov1997@gmail.com>',
-				subject: 'Вопрос руководителю',
-			},
-			(err) => {
-				console.log(err);
-				if (err) {
-					res.status(400);
-					return;
-				}
-				res.status(200);
-			}
-		);
-	}
+    if (req.method === 'POST') {
+        const { name, phone, text } = req.body;
+        client.send(
+            {
+                from: `<${process.env.SMTP_USER}>`,
+                to: `<${process.env.SMTP_USER}>`,
+                subject: 'Вопрос руководителю',
+                attachment: [
+                    {
+                        data: `<html><p>Имя: ${name}</p><p>Телефон: ${phone}</p><p>Коментарий: ${text}</p></html>`,
+                        alternative: true
+                    }
+                ]
+            },
+            (err, mess) => {
+                console.log(err, mess);
+                if (err) {
+                    res.status(400).send();
+                    return;
+                }
+                res.status(200).send();
+            }
+        );
+    }
 }
