52003c4e9c0e3187f3376eaddbf393ff7b8b0fae
Maksim Zhukau <maksimz@checkpoint.com>
2022-07-25 15:57:07 +0300

    Added Tawk to

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
+import TawkTo from 'tawkto-react'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-tabs/style/react-tabs.css';
 import '../styles/index.scss';
 import Layout from '../layouts/Layout';
+import { useEffect } from 'react';
+
+const propertyId =  '62de92e037898912e95f88ae';
+const tawkId = '1g8qmrf5f';
 
 export default function MyApp({ Component, pageProps }) {
+    useEffect(()=> {
+        new TawkTo(propertyId, tawkId)
+    },[]);
     return (
         <Layout>
             <Component {...pageProps} />
