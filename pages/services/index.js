0433340624fd6b2a527dd4abe9397884707b46a8
MaksZhukov <maks_zhukov_97@mail.ru>
2022-07-26 23:01:55 +0300

    Added additional clients, refactored text

--- pages/services/index.js
+++ pages/services/index.js
@@ -6,29 +6,34 @@ import { useRouter } from 'next/router';
 import { services } from '../../config';
 
 export default function Prices() {
-    const router = useRouter();
+	const router = useRouter();
 
-    const handleClick = (href) => () => {
-        router.push('services/' + href);
-    };
-    return (
-        <Container>
-            <div className={styles.services}>
-                {services.map((item, index) => (
-                    <div className={styles.services__item}>
-                        <img className={styles.services__img} alt={item.title} src={item.url}></img>
-                        <div className={styles.reviews__content}>
-                            <h3>{item.title}</h3>
-                            <p>{item.description}</p>
-                            <Button onClick={handleClick(item.href)}>Подробнее</Button>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </Container>
-    );
+	const handleClick = (href) => () => {
+		router.push('services/' + href);
+	};
+	return (
+		<Container>
+			<div className={styles.services}>
+				{services.map((item, index) => (
+					<div key={item.title} className={styles.services__item}>
+						<img
+							className={styles.services__img}
+							alt={item.title}
+							src={item.url}></img>
+						<div className={styles.reviews__content}>
+							<h3>{item.title}</h3>
+							<p>{item.description}</p>
+							<Button onClick={handleClick(item.href)}>
+								Подробнее
+							</Button>
+						</div>
+					</div>
+				))}
+			</div>
+		</Container>
+	);
 }
 
 export async function getStaticProps() {
-    return { props: {} };
+	return { props: {} };
 }
